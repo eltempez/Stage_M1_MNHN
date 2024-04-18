fe1183d42bceb423fe2992eeb5cea6b51262df7c
eltempez <tempez.el@gmail.com>
2024-04-18 13:21:56 +0200

    version finale

--- build_kefir_library.py
+++ build_kefir_library.py
@@ -14,18 +14,25 @@ def get_prefix(file_path, is_paired_illumina = False):
 
 ## Variables
 # list of taxonomy ids
-dict_ncbi = {"B3": 483199, "B4": 318683, "B6": 38307, "B1": 442, "B5": 28448, "B2": 436, "B11": 1597, "B18": 1588, "B10": 152331, "B7": 1245, "B14": 399370, "B13": 82688, "B17": 259059, "B21": 2203724, "B12": 304207, "BB": 5007, "Y3": 4926, "Y1": 4932, "Y2": 48255}
+dict_ncbi = {"B3": 483199, "B4": 318683, "B6": 38307, "B1": 442, "B5": 28448, "B2": 436, "B11": 1597, "B18": 1588, "B10": 152331, "B7": 1245, "B14": 399370, "B13": 82688, "B17": 259059, "B21": 2203724, "B12": 304207, "BB": 5007, "Y3": 4926, "Y1": 4932, "Y2": 48255, "B16": 542, "B15": 336988}
 # directory of .fa/.fasta files
 dir = "/home/eliotttempez/Documents/tests/outputs_snk1/genome_data/fa/"
 
 
 ## Main
 # for each file in the matagenome
-for file in os.listdir(dir):
-    species_name = get_prefix(file)
-    with open(dir + file) as fa_file:
+for file_name in os.listdir(dir):
+    species_name = get_prefix(file_name)
+    file_path = os.path.join(dir, file_name)
+    lines = []
+    with open(file_path, "r") as fa_file:
         for line in fa_file:
             if line.startswith(">"):
-                line += f"|kraken:taxid|{dict_ncbi[species_name]}"
-
+                # Ajouter l'information taxonomique à la ligne d'en-tête
+                line = line.strip() + f"|kraken:taxid|{dict_ncbi[species_name]}\n"
+            lines.append(line)
+    
+    # Réécrire le fichier avec les modifications
+    with open(file_path, "w") as fa_file:
+        fa_file.writelines(lines)
 
